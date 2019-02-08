fc57e20f9f3015f7cd9a1796bb936cdd2d16f0aa
iAmSomeone2 <brendend13@gmail.com>
2019-02-08 14:58:20 -0600

    Add files via upload

---
+++ purgesolidimgs/puregsolidimgs.go
@@ -0,0 +1,7 @@
+/*
+	This program automatically removes any images that are only composed of a single
+	color. This should only be used for removing bad conversions from a neural net
+	upscaler.
+*/
+
+package main
